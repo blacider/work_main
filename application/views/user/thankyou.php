b4aa65c785d3e98ab2ffc0f4613ed03e94f76084
yangboz <YoungWelle@gmail.com>
2014-10-16 19:52:15 +0800

    CHANGED:added view(thankyou.php) with thankyour.php(controller) result handlers;

---
+++ application/views/user/thankyou.php
@@ -0,0 +1,7 @@
+<div id="headerwrap">
+    <div class="container">
+        <div class="row">
+            <div class="alert alert-success" role="alert">Well done,thank you!</div>
+        </div><!-- /row -->
+    </div><!-- /container -->
+</div><!-- /headerwrap -->
