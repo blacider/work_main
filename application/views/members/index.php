c0bad57ac4e6db40db4c74e00bae8fe321dda10e
瞿庆义 <qingyi.qu@cloudbaoxiao.com>
2015-07-03 11:02:30 +0800

    多级部门展示

--- application/views/members/index.php
+++ application/views/members/index.php
@@ -8,14 +8,31 @@
 <div class="page-content">
 <div class="page-content-area">
 <div class="row">
-<div class="col-xs-3">
+<!-- <div class="col-xs-3">
     <div class="panel panel-primary">
         <div class="panel-heading"><h3 class="panel-title default">组织架构</h3></div>
         <div class="panel-body">
             <div id="grouptree" class="tree"></div>
         </div>
     </div>
-</div>
+</div> -->
+
+
+
+<div class="col-sm-3">
+        <div class="widget-box widget-color-blue">
+                <div class="widget-header">
+                    <h4 class="widget-title lighter smaller">组织结构</h4>
+                    </div>
+
+                    <div class="widget-body">
+                        <div class="widget-main padding-8">
+                            <div id="tree2" class="tree"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
 <div class="col-xs-9">
     <!--
     <table id="grid-table"></table>
@@ -257,7 +274,7 @@ function bind_event() {
         }
     });
 }
-$(document).ready(function(){
+/*$(document).ready(function(){
     bind_event();
     $.ajax({
         url: __BASE + "/members/listtreegroup",
@@ -301,5 +318,202 @@ $(document).ready(function(){
 
             }
     });
-});
+});*/
 </script>
+
+<script language="javascript">
+var __BASE = "<?php echo $base_url; ?>";
+</script>
+<script type="text/javascript">
+
+        Array.prototype.remove=function(obj){ 
+            for(var i =0;i <this.length;i++){ 
+                var temp = this[i]; 
+                if(!isNaN(obj)){ 
+                temp=i; 
+            } 
+            if(temp == obj){ 
+                for(var j = i;j <this.length;j++){ 
+                this[j]=this[j+1]; 
+            } 
+                this.length = this.length-1; 
+                } 
+            } 
+            } 
+    var scripts = [null,"/static/ace/js/fuelux/fuelux.tree.min.js", null]
+    ace.load_ajax_scripts(scripts, function() {
+      //inline scripts related to this page
+         jQuery(function($){
+         $.ajax({
+            url:__BASE+ "/members/getgroups",
+            method:'GET',
+            dataType:'json',
+            success:function(data){
+
+                    var ace_icon = ace.vars['icon'];
+                    //class="'+ace_icon+' fa fa-file-text grey"
+                    //becomes
+                    //class="ace-icon fa fa-file-text grey"
+                    console.log(data);
+                    var js_data = {};
+                    var obj = new Array();
+                    var unroot = new Array();
+                   // js_data['全体员工'] = {name:'全体员工'，id:'-2'，type:'folder','icon-class':'red'};
+                    for(var i = 0 ; i < data.length ; i++)
+                    {
+
+                        if(data[i]['pid'] == "0")
+                        {
+                            js_data[data[i]['name']] = {name: data[i]['name'], id:data[i]['id'] ,type: 'folder', 'icon-class':'red'};   
+                            var item = js_data[data[i]['name']];
+                            item['additionalParameters']={'children':{}};
+                           /* item['additionalParameters']['children']['members']['additionalParameters']={};
+                            var param = item['additionalParameters']['children']['members']['additionalParameters'];
+                            param['children'] = [];
+                            var mem = param['children'];
+                            for(var j = 0; j< data[i]['members'].length; j++)
+                            {
+                                var temp = {name: '<i class="'+ace_icon+' fa fa-users blue"></i>'+data[i]['members'][j]['nickname'],type:'item'};
+                                mem.push(temp);
+                            }*/
+
+                            obj.push({id:data[i]['id'],'item':js_data[data[i]['name']]});
+
+                        }
+                        else if(data[i]['pid'] > 0)
+                        {
+                            unroot.push(data[i]);
+                        }
+                    }
+                    //寻找子部门
+                    while(unroot.length!=0)
+                    {
+                        var tempobj = new Array();
+                        for(var num = 0; num < obj.length;num++)
+                        {
+                            for(var unum = 0 ; unum < unroot.length ; unum++)
+                            {
+                                if(unroot[unum]['pid'] == obj[num]['id'])
+                                {
+
+                                      obj[num]['item']['additionalParameters']['children'][unroot[unum]['name']]={name:unroot[unum]['name'],id:unroot[unum]['id'], type: 'folder', 'icon-class':'pink'};
+                                      var tempitem = obj[num]['item']['additionalParameters']['children'][unroot[unum]['name']];
+                                      tempitem['additionalParameters'] = {'children':{}};
+                                   //   tempitem['additionalParameters']['children']['members']['additionalParameters']={};
+                                   //   var param = tempitem['additionalParameters']['children']['members']['additionalParameters'];
+                                      //param['children'] = [];
+                                     // var mem = param['children'];
+
+                                  /*    for(var j = 0; j< unroot[unum]['members'].length; j++)
+                                        {
+                                            var temp = {name: '<i class="'+ace_icon+' fa fa-users blue"></i>'+unroot[unum]['members'][j]['nickname'],type:'item'};
+                                            mem.push(temp);
+                                        }*/
+                                        tempobj.push({id:unroot[unum]['id'],'item':tempitem});
+                                      unroot.remove(unum);
+                                }
+                            }
+                            console.log("#tempobj:");
+                            console.log(tempobj);
+                            console.log("#obj");
+                            console.log(obj);
+                        }
+
+                        obj = tempobj;
+                    } 
+
+                    js_data['全体员工'] = {name: '全体员工', id:'-2' ,type: 'folder', 'icon-class':'red'}; 
+                    
+                    js_data['已邀请'] = {name: '已邀请', id:'-1' ,type: 'folder', 'icon-class':'red'}; 
+
+                    var treeDataSource = new DataSourceTree({data: js_data});
+
+                    $('#tree2').ace_tree({
+                        dataSource: treeDataSource ,
+                        loadingHTML:'<div class="tree-loading"><i class="ace-icon fa fa-refresh fa-spin blue"></i></div>',
+                        'open-icon' : 'ace-icon fa fa-cog',
+                        'close-icon' : 'ace-icon fa fa-cogs',
+                        'selectable' : true,
+                        'selected-icon' : null,
+                        'unselected-icon' : null
+                    });
+                    
+                    
+                    $('#tree2')
+                    .on('updated', function(e, result) {
+                        //result.info  >> an array containing selected items
+                        //result.item
+                        //result.eventType >> (selected or unselected)
+                    })
+                    .on('selected', function(e) {
+                        console.log("group selected");
+                    })
+                    .on('unselected', function(e) {
+                    })
+                    .on('opened', function(e,result) {
+                        //var _gid = _data;
+                        if(result.id != undefined)
+                        {
+                            var _gid = result.id;
+                            console.log(_gid);
+                            load_group(_gid);
+                        }
+                    })
+                    .on('closed', function(e) {
+                    });
+
+            },
+            error:function(){
+                console.log("error");
+            }
+
+    });
+
+        /**
+        $('#tree1').on('loaded', function (evt, data) {
+        });
+
+        $('#tree1').on('opened', function (evt, data) {
+        });
+
+        $('#tree1').on('closed', function (evt, data) {
+        });
+
+        $('#tree1').on('selected', function (evt, data) {
+        });
+        */
+});
+    });
+</script>
+<script type="text/javascript">
+    
+    var DataSourceTree = function(options) {
+    this._data  = options.data;
+    this._delay = options.delay;
+}
+
+DataSourceTree.prototype.data = function(options, callback) {
+    var self = this;
+    var $data = null;
+
+    if(!("name" in options) && !("type" in options)){
+        $data = this._data;//the root tree
+        callback({ data: $data });
+        return;
+    }
+    else if("type" in options && options.type == "folder") {
+        if("additionalParameters" in options && "children" in options.additionalParameters)
+            $data = options.additionalParameters.children;
+        else $data = {}//no data
+    }
+    
+    if($data != null)//this setTimeout is only for mimicking some random delay
+        setTimeout(function(){callback({ data: $data });} , parseInt(Math.random() * 500) + 200);
+
+    //we have used static data here
+    //but you can retrieve your data dynamically from a server using ajax call
+    //checkout examples/treeview.html and examples/treeview.js for more info
+};
+
+</script>
+
