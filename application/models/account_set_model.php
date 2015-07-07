1cd23c33da3d136efb299192f9996a4cd1343c0d
瞿庆义 <qingyi.qu@cloudbaoxiao.com>
2015-07-07 11:02:11 +0800

    添加帐套管理

---
+++ application/models/account_set_model.php
@@ -0,0 +1,52 @@
+<?php
+	class Account_Set_Model extends Reim_Model{
+		public function get_account_set_list()
+		{
+			$jwt = $this->session->userdata('jwt');
+			if(!$jwt) return false;
+			$url = $this->get_url('sob/full');
+			$buf=$this->do_Get($url,$jwt);
+			$obj = json_decode($buf,true);
+			log_message("debug","###########ACOUNT_SETS****:$buf");
+			return $obj;
+		}
+		public function create_account_set($name,$gids)
+		{
+			$jwt = $this->session->userdata('jwt');
+			if(!$jwt) return false;
+			$data = array(
+				'name' => $name,
+				'dids' => $gids
+			);
+			$url = $this->get_url('sob');
+			$buf = $this->do_Post($url,$data,$jwt);
+			$obj = json_decode($buf,true);
+			return $obj;
+		}
+		public function update_account_set($id,$name,$gids)
+		{
+			$jwt = $this->session->userdata('jwt');
+			if(!$jwt) return false;
+			$data = array(
+				'id' => $id,
+				'name' => $name,
+				'dids' => $gids
+			);
+			$url = $this->get_url('sob');
+			$buf = $this->do_Put($url,$data,$jwt);
+			$obj = json_decode($buf,true);
+			return $obj;	
+		}
+		public function delete_account_set($id)
+		{
+			$jwt = $this->session->userdata('jwt');
+			if(!$jwt) return false;
+			
+			$data = array();
+			$url = $this->get_url("sob/$id");
+			log_message("debug","####$url");
+			$buf = $this->do_Delete($url,$data,$jwt);
+			return $buf;
+		}
+	}
+?>
