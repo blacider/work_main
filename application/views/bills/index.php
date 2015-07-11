0b79be5ecda3faaec7a5bac87fcbba34101b5e98
lichangyuan <changyuan.li@cloudbaoxiao.com>
2015-07-11 18:46:06 +0800

    temporary

--- application/views/bills/index.php
+++ application/views/bills/index.php
@@ -15,14 +15,14 @@
     <div class="modal-content">
       <div class="modal-header">
         <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-        <h4 class="modal-title">Modal title</h4>
+        <h4 class="modal-title">支付以下报告</h4>
       </div>
       <div class="modal-body">
         <p>One fine body&hellip;</p>
       </div>
       <div class="modal-footer">
-        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
+        <button type="button" class="btn btn-default" data-dismiss="modal">取消</button>
+        <button type="button" class="btn btn-primary">支付</button>
       </div>
     </div><!-- /.modal-content -->
   </div><!-- /.modal-dialog -->
