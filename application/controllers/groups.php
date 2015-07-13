b29f42fddc4ef942dddcf416caad36477ea17077
superserver <y@cloudbaoxiao.com>
2015-07-13 19:55:35 +0800

    新建部门

--- application/controllers/groups.php
+++ application/controllers/groups.php
@@ -16,8 +16,8 @@ class Groups extends REIM_Controller {
         $uids = $this->input->post('uids');
         $pid = $this->input->post('pgroup');
         $manager = $this->input->post('manager');
-        $code = $this->input->post('code');
         $uids = implode(",", $uids);
+        $info = $this->ug->create_group($uids, $name,$code,$pid,$manager);
            redirect(base_url('members/groups'));
     }
 
