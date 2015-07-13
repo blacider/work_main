7cfaac6fc7f1cd3688171dd40932c2d6384f34f5
lichangyuan <changyuan.li@cloudbaoxiao.com>
2015-07-13 18:34:09 +0800

    add operation flow

--- application/views/items/view.php
+++ application/views/items/view.php
@@ -80,6 +80,28 @@
                                 </div>
                             </div>
 
+
+                            <div class="form-group">
+                                <label class="col-sm-1 control-label no-padding-right">修改记录</label>
+                                <div class="col-xs-9 col-sm-9">
+                                    <table class="table table-bordered table-striped">
+                                        <tr>
+                                            <td>修改人</td>
+                                            <td>修改时间</td>
+                                            <td>操作</td>
+                                        </tr>
+                                        <?php foreach($flow as $i){ ?>
+                                        <tr>
+                                            <td><?php echo $i['operator']; ?></td>
+                                   
+                                            <td><?php echo $i['optdate']; ?></td>
+                                            <td><?php echo $i['operation']; ?></td>
+                                        </tr>
+                                        <?php } ?>  
+                                    </table>
+                                </div>
+                            </div>
+
                             <div class="clearfix form-actions col-sm-8 col-xs-8">
                                 <div class="col-md-offset-3 col-md-6">
                                     <?php 
