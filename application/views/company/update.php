6a6df8c8afc91fb6d62796e0cf629e0683e9b67d
superserver <y@cloudbaoxiao.com>
2015-07-16 12:56:20 +0800

    删除注释

--- application/views/company/update.php
+++ application/views/company/update.php
@@ -207,7 +207,7 @@
         </form>
     </div>
 </div>
-<p><?php echo json_encode($cate_arr)?></p>
+<!--<p><?php echo json_encode($cate_arr)?></p> -->
 <script type="text/javascript">
     var freq = "<?php echo $rule['freq_count']?>";
     var all_members = "<?php echo $rule['all_company']?>";
