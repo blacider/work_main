dd49fba62b756f089d3da5f20b2be4c3dcb84581
niurenpeng <niurenpeng@niurenpeng.com>
2015-07-21 17:59:48 +0800

    cc冲突

--- application/config/database.php
+++ application/config/database.php
@@ -49,8 +49,8 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = 'admin';
-$db['default']['password'] = 'xODJhZjY2MzQ4ZfdsaTBjMjMzMjZlZWM2OTFmN';
+$db['default']['username'] = 'root';
+$db['default']['password'] = 'xudan0227';
 $db['default']['database'] = 'reimadmin';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
@@ -65,8 +65,8 @@ $db['default']['autoinit'] = TRUE;
 $db['default']['stricton'] = FALSE;
 
 $db['data']['hostname'] = 'localhost';
-$db['data']['username'] = 'admin';
-$db['data']['password'] = 'xODJhZjY2MzQ4ZfdsaTBjMjMzMjZlZWM2OTFmN';
+$db['default']['username'] = 'root';
+$db['default']['password'] = 'xudan0227';
 $db['data']['database'] = 'reim';
 $db['data']['dbdriver'] = 'mysql';
 $db['data']['dbprefix'] = '';
