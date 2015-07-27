1d9fcbf160a895575998086c26b442b26ce8ea84
niurenpeng <niurenpeng@niurenpeng.com>
2015-07-27 15:29:24 +0800

    隐藏部门列

--- static/js/bills.js
+++ static/js/bills.js
@@ -68,7 +68,7 @@ jQuery(grid_selector).jqGrid({
     {name:'amount',index:'amount', sorttype: myCustomSort,width:70, editable: false,editoptions: {size:"20",maxlength:"30"},unformat: aceSwitch,search:false},
     {name:'status_str',index:'status_str', width:70, editable: true,edittype:"select",editoptions: {value:"4:通过;3:拒绝"},unformat: aceSwitch,search:false},
     {name:'options',index:'options', width:70, editable: true,edittype:"select",editoptions: {value:"4:通过;3:拒绝"},unformat: aceSwitch,search:false},
-    {name:'ugs', index:'ugs', width:50,editable: false,editoptions:{size:"20",maxlength:"30"}},
+    {name:'ugs', index:'ugs', width:50,editable: false,editoptions:{size:"20",maxlength:"30"},hidden:true},
 
 
     ], 
