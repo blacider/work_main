5777764fe91df95dd48945b199590b2cf319b150
superserver <y@cloudbaoxiao.com>
2015-07-28 14:29:22 +0800

    修复页面返回json为空时的bug

--- application/views/items/new.php
+++ application/views/items/new.php
@@ -194,7 +194,14 @@
 <script language="javascript">
 var __BASE = "<?php echo $base_url; ?>";
 var config = '<?php echo $_config?>';
-var __config = JSON.parse(config);
+if(config != '')
+{
+    var __config = JSON.parse(config);
+}
+else
+{
+    var __config = '';
+}
 
 var not_auto_note = "";
 var flag = 0;
