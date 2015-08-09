0077d95c5b06ae8422c89cfba59880ad1141752e
niurenpeng <niurenpeng@niurenpeng.com>
2015-08-09 12:39:33 +0800

    删除员工前面增加名字

--- application/views/members/delmembers.php
+++ application/views/members/delmembers.php
@@ -7,13 +7,14 @@
                         	<div class="form-group">
                         	<label class="col-sm-1 control-label no-padding-rigtht" style="color:red">员工</label>
                             <div class="col-xs-4 col-sm-4">
-                                <select id="member" class="chosen-select range tag-input-style" multiple="multiple" name="member[]"  data-placeholder="请选择员工">
+                                <select id="member" class="chosen-select tag-input-style" style="width:95%" multiple="multiple" name="member[]"  data-placeholder="请选择员工">
                                     <?php
                                     foreach($members as $ug){
                                     ?>
                                     <option  value="<?php echo $ug['id']; ?>
                                         ">
-                                        <?php echo $ug['email']; ?></option>
+                                        <?php echo $ug['nickname']; ?>
+                                        (<?php echo $ug['email']; ?>)</option>
                                     <?php
                                     }
 
@@ -24,7 +25,7 @@
 
                         	<div class="form-group" style="clear:both">
                         		<label class="col-sm-2 control-label no-padding-right">结果:</label>
-                        		
+
                         	</div>
                         		
                         		
@@ -35,7 +36,7 @@
 </div>
 <script type="text/javascript">
 	$(document).ready(function(){
-		$('.chosen-select').chosen({width: "100%"}); 
+		$('.chosen-select').chosen(); 
 		$('#submit').click(function(event) {
 			var members = $('#members').val();
 		});
