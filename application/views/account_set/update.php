6ce33abbd82958bf1254e4979cf04bf0554af987
niurenpeng <niurenpeng@niurenpeng.com>
2015-08-10 09:52:13 +0800

    增加说明和是否需要附件

--- application/views/account_set/update.php
+++ application/views/account_set/update.php
@@ -25,6 +25,14 @@
                             <div class="col-xs-3 col-sm-3">
                                 <input id="sob_name" type="text" class="form-controller col-xs-12" name="sob_name" placeholder="输入帐套名"></div>
                         </div>
+
+                        
+
+                        <div class="form-group">
+                            <label class="col-sm-2 control-label no-padding-rigtht">说明</label>
+                            <div class="col-xs-3 col-sm-3">
+                                <input id="sob_name" type="text" class="form-controller col-xs-12" name="sob_name" placeholder="输入说明"></div>
+                        </div>
                         <label style="left:0;position: absolute;" class="col-sm-2 control-label no-padding-rigtht">类目</label>
                         <script type="text/javascript">
                             var _subSob = [];
@@ -122,7 +130,19 @@
                         <div class="form-group">
                             <div style="border-radius:10px;" onclick="addCate(this.parentNode)" class="col-sm-1 col-xs-1 col-sm-offset-2 col-xs-offset-2 btn-primary addDrop">添加+</div>    
                         </div>
-                        
+                        <div class="form-group">
+                                <label class="col-sm-3 control-label no-padding-right">是否需要附件</label>
+                                <div class="col-xs-6 col-sm-6">
+                                 <!--   <input type="text" placeholder="组名称" class="col-xs-12" required="required" name="gname"> -->
+                                   <!-- <div class="col-xs-12 col-sm-12 col-md-12"> -->
+                                        <label style="margin-top:8px;">
+                                            <input name="need_bank_info" class="ace ace-switch btn-rotate" type="checkbox" id="need_bank_info" style="margin-top:4px;" />
+                                            <span class="lbl"></span>
+                                        </label>
+
+                                   <!-- </div> -->
+                                </div>
+                        </div>
                         <label class="col-sm-2 control-label no-padding-rigtht" style="position:absolute;left:0px;">适用范围</label>
                         <div class="form-group">
                             <div class="col-xs-1 col-sm-1 col-sm-offset-2 col-xs-offset-2">
