72bd1561b155037ce32ec504678a9bd18a07d047
superserver <y@cloudbaoxiao.com>
2015-08-11 19:05:06 +0800

    修正创建报告时候的小数点,修复访问权限‘

--- application/views/reports/new.php
+++ application/views/reports/new.php
@@ -149,6 +149,34 @@ echo $buf;
 </div>
 <script language="javascript">
 var __BASE = "<?php echo $base_url; ?>";
+function toDecimal(x) {  
+    var f = parseFloat(x);  
+    if (isNaN(f)) {  
+        return;  
+    }  
+    f = Math.round(x*100)/100;  
+    return f;  
+}  
+//制保留2位小数，如：2，会在2后面补上00.即2.00  
+function toDecimal2(x) {  
+    var f = parseFloat(x);  
+    if (isNaN(f)) {  
+        return false;  
+    }  
+    var f = Math.round(x*100)/100;  
+    var s = f.toString();  
+    var rs = s.indexOf('.');  
+    if (rs < 0) {  
+        rs = s.length;  
+        s += '.';  
+    }  
+    while (s.length <= rs + 2) {  
+        s += '0';  
+    }  
+    return s;  
+}  
+
+
 $(document).ready(function(){
     //var now = moment();
     $('#date-timepicker1').datetimepicker({
@@ -213,6 +241,7 @@ $(document).ready(function(){
             });
            // $("[name='item[]']").prop('checked',false);
         }
+        update_tamount();
      });
 
 
@@ -276,6 +305,6 @@ function update_tamount(){
             sum+=amount;
         };
     });
-    $('#tamount').html(sum);
+    $('#tamount').html('￥' + toDecimal2(sum));
 }
 </script>
