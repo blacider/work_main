998b6233233d56ad2af7e72749d4203137adb7fd
niurenpeng <niurenpeng@niurenpeng.com>
2015-08-11 13:46:54 +0800

    前端修改

--- application/views/account_set/update.php
+++ application/views/account_set/update.php
@@ -327,23 +327,19 @@
             <label class="col-sm-2 col-xl-2">名称</label>
             <input type="text" name='name' data-placeholder="请输入名称"></div>
         <div class="form-group">
-                            <label class="col-sm-2 control-label no-padding-rigtht">说明</label>
-                            <div class="col-xs-3 col-sm-3">
-                                <input id="sob_name" type="text" class="form-controller col-xs-12" name="sob_name" placeholder="输入说明"></div>
+            <label class="col-sm-2 control-label no-padding-rigtht">说明</label>
+                            
+            <input id="sob_name" type="text" name="sob_name" placeholder="输入说明">
         </div>
-        <div class="form-group">
-                                <label class="col-sm-3 control-label no-padding-right">是否需要附件</label>
+        <div class="form-group" style="height:30px">
+            <label class="col-sm-3 control-label no-padding-right">是否需要附件</label>
                                 <div class="col-xs-6 col-sm-6">
-                                 <!--   <input type="text" placeholder="组名称" class="col-xs-12" required="required" name="gname"> -->
-                                   <!-- <div class="col-xs-12 col-sm-12 col-md-12"> -->
                                         <label style="margin-top:8px;">
                                             <input name="need_bank_info" class="ace ace-switch btn-rotate" type="checkbox" id="need_bank_info" style="margin-top:4px;" />
                                             <span class="lbl"></span>
                                         </label>
-
-                                   <!-- </div> -->
                                 </div>
-                        </div>
+        </div>
         <div class="form-group" style="height:30px">
             <label class="col-sm-2 col-xl-2">图片</label>
             <div class="dropdown col-sm-3 col-xl-3">
@@ -402,25 +398,21 @@
         <div class="form-group">
             <label class="col-sm-2 col-xl-2">名称</label>
             <input type="text" name='name' data-placeholder="请输入名称"></div>
-        <div class="form-group" style="height:30px">
         <div class="form-group">
-                            <label class="col-sm-2 control-label no-padding-rigtht">说明</label>
-                            <div class="col-xs-3 col-sm-3">
-                                <input id="sob_name" type="text" class="form-controller col-xs-12" name="sob_name" placeholder="输入说明"></div>
-                        </div>
-                        <div class="form-group">
-                                <label class="col-sm-3 control-label no-padding-right">是否需要附件</label>
+            <label class="col-sm-2 control-label no-padding-rigtht">说明</label>
+                            
+            <input id="sob_name" type="text" name="sob_name" placeholder="输入说明">
+        </div>
+        <div class="form-group" style="height:30px">
+            <label class="col-sm-3 control-label no-padding-right">是否需要附件</label>
                                 <div class="col-xs-6 col-sm-6">
-                                 <!--   <input type="text" placeholder="组名称" class="col-xs-12" required="required" name="gname"> -->
-                                   <!-- <div class="col-xs-12 col-sm-12 col-md-12"> -->
                                         <label style="margin-top:8px;">
                                             <input name="need_bank_info" class="ace ace-switch btn-rotate" type="checkbox" id="need_bank_info" style="margin-top:4px;" />
                                             <span class="lbl"></span>
                                         </label>
-
-                                   <!-- </div> -->
                                 </div>
-                        </div>
+        </div>
+        <div class="form-group" style="height:30px">
             <label class="col-sm-2 col-xl-2">图片</label>
             <div class="dropdown col-sm-3 col-xl-3">
                 <div class="dropdown-toggle down-image" data-toggle="dropdown" id="dropdownMenuImg_">
