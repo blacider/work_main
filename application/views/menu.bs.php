4aea6a4eebd47eb5d6a398e530df7170af30d08e
Yang Song <song.yang@yunbaoxiao.com>
2015-09-15 22:36:40 +0800

    修改菜单顺序

--- application/views/menu.bs.php
+++ application/views/menu.bs.php
@@ -291,21 +291,21 @@ if($profile['admin'] == 1 || $profile['admin'] == 3){
         <b class="arrow"></b>
 
         </li>
-
-         <li class="hsub" id="delmembers">
-        <a href="<?php echo base_url('members/delmembers'); ?>" >
+        <li class="hsub" id="add">
+        <a href="<?php echo base_url('members/add'); ?>" >
             <i class="menu-icon fa fa-caret-right"></i>
-            删除员工
+            添加部门
         </a>
 
         <b class="arrow"></b>
 
         </li>
 
-        <li class="hsub" id="add">
-        <a href="<?php echo base_url('members/add'); ?>" >
+
+         <li class="hsub" id="delmembers">
+        <a href="<?php echo base_url('members/delmembers'); ?>" >
             <i class="menu-icon fa fa-caret-right"></i>
-            添加部门
+            删除员工
         </a>
 
         <b class="arrow"></b>
