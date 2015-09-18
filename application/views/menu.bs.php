6960b45d656d9f2653342f8fd57482a5279b0c83
Yang Song <song.yang@yunbaoxiao.com>
2015-09-18 18:02:08 +0800

    看上去可以了，准备合并到dev

--- application/views/menu.bs.php
+++ application/views/menu.bs.php
@@ -367,8 +367,8 @@ if($profile['admin'] == 1 || $profile['admin'] == 3){
         <b class="arrow"></b>
         </li>
 
-        <li class="hsub" id="account_set">
-        <a href="<?php echo base_url('category/export'); ?>" > <i class="menu-icon fa fa-caret-right"></i> 账套导入/导出 </a>
+        <li class="hsub" id="cexport">
+        <a href="<?php echo base_url('category/cexport'); ?>" > <i class="menu-icon fa fa-caret-right"></i> 账套导入/导出 </a>
         <b class="arrow"></b>
         </li>
 <!--
@@ -380,10 +380,6 @@ if($profile['admin'] == 1 || $profile['admin'] == 3){
 
         <li class="hsub" id="tags">
         <a href="<?php echo base_url('category/tags'); ?>" > <i class="menu-icon fa fa-caret-right"></i> 标签管理 </a>
-        <b class="arrow"></b>
-        </li>
-        <li class="hsub" id="tags">
-        <a href="<?php echo base_url('category/exports'); ?>" > <i class="menu-icon fa fa-caret-right"></i> 导入帐套 </a>
         <b class="arrow"></b>
         </li>
     </ul>
