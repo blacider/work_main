a9ad57bd81499f3459ec16bd192b50245fb8fb67
瞿庆义 <qingyi.qu@cloudbaoxiao.com>
2015-09-22 21:24:08 +0800

    特殊情况处理

--- static/js/finance_flow.js
+++ static/js/finance_flow.js
@@ -12,11 +12,17 @@ function chose_others_zero(item,nextId) {
         }
     }
     $('#modal_next').modal('show');
-
-    $('#modal_managers').val(nextId).prop('selected',true);
-    $('#modal_managers').trigger('chosen:updated');
-    $('#modal_managers').attr('disabled',true);
-    
+    console.log(nextId);
+    if(nextId.length)
+    {
+            $('#modal_managers').val(nextId[0]).prop('selected',true);
+            $('#modal_managers').trigger('chosen:updated');
+    }
+    else
+    {
+            $('#mypass').attr('disabled',true).trigger('chosen:updated');
+    }
+            $('#modal_managers').attr('disabled',true).trigger('chosen:updated');
 }
 function chose_others(_id) {
     $('#modal_next_').modal('hide');
@@ -46,14 +52,10 @@ function bind_event(){
                         getData = data['data'].suggestion;
                         if (data['data'].complete == 0) {
                             $('#rid').val(_id);
-                            console.log('还不能直接结束');
                             chose_others_zero(_id,data['data'].suggestion);
-                             
                         } else {
                             $('#rid_').val(_id);
-                            
-                            console.log('还不能直接结束');
-                                $('#modal_next_').modal('show'); 
+                            $('#modal_next_').modal('show'); 
                         }
                     }
                 }
