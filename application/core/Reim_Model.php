be35acf96ba58b5f4a49db807661b58e516d6ff4
George Xie <georgexsh@gmail.com>
2015-10-21 03:32:51 +0800

    php5.5以上才存在CURLOPT_SAFE_UPLOAD, 要在加入FIELDS前设置

--- application/core/Reim_Model.php
+++ application/core/Reim_Model.php
@@ -165,6 +165,11 @@ class Reim_Model extends CI_Model {
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); 
         curl_setopt($ch, CURLOPT_POST, count($fields)) ;
         curl_setopt($ch, CURLOPT_USERAGENT, $this->get_user_agent());
+        if (PHP_VERSION_ID > 050500){
+            # Added in PHP 5.5.0
+            # see also http://comments.gmane.org/gmane.comp.php.devel/87521
+            curl_setopt($ch, CURLOPT_SAFE_UPLOAD, false);
+        }
         //curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_POSTFIELDS, $fields);
         //if($force_bin == 1){
@@ -174,8 +179,6 @@ class Reim_Model extends CI_Model {
             log_message("debug", json_encode($extraheader));
             curl_setopt($ch, CURLOPT_HTTPHEADER, $extraheader);
         }
-        # see http://comments.gmane.org/gmane.comp.php.devel/87521
-        curl_setopt($ch, CURLOPT_SAFE_UPLOAD, false);
         curl_setopt($ch, CURLOPT_VERBOSE, true) ; // 在启用 CURLOPT_RETURNTRANSFER 时候将获取数据返回
         ob_start();
         curl_exec($ch );
