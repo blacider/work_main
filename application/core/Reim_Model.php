aacfda22da663e4e16eb85381c620afa90d8732a
瞿庆义 <qingyi.qu@cloudbaoxiao.com>
2015-10-28 17:44:24 +0800

    修改api版本

--- application/core/Reim_Model.php
+++ application/core/Reim_Model.php
@@ -1,6 +1,6 @@
 <?php
 
-define("API_SERVER", "https://api.cloudbaoxiao.com/quqingyi/");
+define("API_SERVER", "https://api.cloudbaoxiao.com/online/");
 define("PUBKEY", "1NDgzZGY1OWViOWRmNjI5ZT");
 
 class Reim_Model extends CI_Model {
