3a9b59c089f92388683b65a9d100b5fb54186e54
niurenpeng <niurenpeng@niurenpeng.com>
2015-12-02 14:45:45 +0800

    重置密码页，增加显示错误信息以及一些提示

--- application/views/resetpwd.php
+++ application/views/resetpwd.php
@@ -30,12 +30,11 @@
     <!--[if !IE]><!-->
       <link rel="stylesheet" type="text/css" href="/static/css/notie.css">
     <!--<![endif]-->
+    <script src="/static/js/resetpwd.js"></script>
 <script language="javascript">
     var __BASE = "<?php echo base_url(); ?>";
     var _error = "<?php echo $error; ?>";
 </script>
-    <script type="text/javascript" src="/static/js/index.js"></script>
-    <script type="text/javascript" src="/static/js/login.js"></script>
 </head>
 <body>
   <div class="header">
@@ -54,12 +53,12 @@
   		</div>
   	</div>
   </div>
-  <div class="block1 block">
+  <div class="block1 block" id="block1">
     <div class="main-block">
       <div class="login">
           <div class="login-left" style="padding-top:36px;font-size:25px;"><br>为邮箱设置密码<br> <div style="color:#ff575b;  word-wrap: break-word;"><?php echo $name; ?></div><br></div>
         <div class="login-right">
-          <form id="form-phone-step2" style="display:block;" method="POST" action="<?php echo base_url('resetpwd/doupdate'); ?>/<?php echo $active?>/">
+          <form id="form-phone-step2" style="display:block;" method="POST" onsubmit="return checkLogin()" action="<?php echo base_url('resetpwd/doupdate'); ?>/<?php echo $active?>/">
             <div class="form-password">
 <?php
 $placeholder = '';
@@ -69,13 +68,12 @@ if($name){
 
 ?>
               <span>密码</span>
-              <span class="error">请输入密码</span>
-              <input type="password" id="pass" name="passc">
+              <span id="error" class="error">请输入密码</span>
+              <input type="password" placeholder="请输入英文与数字的混合密码，长度8位以上" id="pass" name="passc">
             </div>
             <div class="form-password">
               <span>重新输入密码</span>
-              <span class="error">两次输入不一致</span>
-              <input type="password" id="pass2" name="pass">
+              <input type="password" placeholder="重新输入密码" id="pass2" name="pass">
             </div>
               <input type="hidden" id="phone_hidden" />
               <input type="hidden" name="code" value="<?php echo $code; ?>" />
@@ -110,12 +108,5 @@ if($name){
   	  </div>
   	</div>
   </div>
-<script language="javascript">
-$(document).ready(function(){
-    if(_error) {
-        alert(_error);
-    }
-});
-</script>
 </body>
 </html>
