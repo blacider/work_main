c9932cdf3fcb8c0f47bd81cd6130b7b39cfda29d
瞿庆义 <qingyi.qu@cloudbaoxiao.com>
2015-12-11 18:50:06 +0800

    标签模块

--- application/views/module/items/item_tags.php
+++ application/views/module/items/item_tags.php
@@ -1,7 +1,7 @@
 <div class="form-group">
 <label class="col-sm-1 control-label no-padding-right"><?php echo $item_customization_value['title'];?></label>
 <div class="col-xs-6 col-sm-6">
-<select class="chosen-select tag-input-style" name="tags[]" multiple="multiple" data-placeholder="请选择标签">
+<select class="chosen-select tag-input-style" id="item_tags" name="tags[]" multiple="multiple" data-placeholder="请选择标签">
 <?php foreach($tags as $category) {?>
 
 <option value="<?php echo $category['id']; ?>"><?php echo $category['name']; ?></option>
@@ -10,3 +10,19 @@
 
 </div>
 </div>
+<script type="text/javascript">
+	function init_tags_module()
+	{
+		var tag_list = item_info['tag_ids'].split(',');
+		console.log(tag_list);
+		$('#item_tags').val(tag_list).prop('selected',true);
+		$('#item_tags').trigger('change');
+		$('#item_tags').trigger('chosen:updated');
+	}
+	$(document).ready(function(){
+		if(PAGE_TYPE != 0)
+		{
+			init_tags_module();
+		}
+	});
+</script>
