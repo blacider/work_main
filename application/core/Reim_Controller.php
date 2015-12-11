e110d7c2c2c2dee00f3c0e2c6a1cc6f53ed2fdc3
瞿庆义 <qingyi.qu@cloudbaoxiao.com>
2015-12-11 18:31:09 +0800

    删除定义在Reim_Controller中的html_container函数

--- application/core/Reim_Controller.php
+++ application/core/Reim_Controller.php
@@ -58,20 +58,6 @@ class REIM_Controller extends CI_Controller{
         return false;
     }
 
-    public function get_html_container($content,$html_id='reim_html_id',$is_hidden=true)
-    {
-        $is_hidden_string = 'hidden';
-        if(!$is_hidden)
-        {
-            $is_hidden_string = '';
-        }
-        
-        if(!is_string($content)) 
-        {
-            $content = json_encode($content);
-        }
-        return "<div " . $is_hidden_string ." id='" . htmlspecialchars($html_id) . "' data-value= '" . htmlspecialchars($content) . "' >" . htmlspecialchars($content) . "</div>";
-    }
 
     public function  eload($view_name, $custom_data, $menu_page = 'menu.php'){
         $this->load->model('user_model');
