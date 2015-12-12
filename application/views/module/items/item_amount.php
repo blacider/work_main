9660d9121a44cc480b0b6ea852d9c0fb39934a9d
瞿庆义 <qingyi.qu@cloudbaoxiao.com>
2015-12-12 18:39:17 +0800

    拆分货币的js

--- application/views/module/items/item_amount.php
+++ application/views/module/items/item_amount.php
@@ -1,128 +1,51 @@
-<?php
-    if($company_config['open_exchange'])
-    {
-?>
 <div class="form-group">
     <label class="col-sm-1 control-label no-padding-right"><?php echo $item_customization_value['title']?></label>
     <div class="col-xs-6 col-sm-6">
         <div class="input-group input-group">
             <span class="input-group-addon" id='coin_simbol'>￥</span>
             <input type="number" class="form-controller col-xs-12 col-sm-12" name="amount" id="amount" placeholder="金额" required>
+        <?php 
+        if($company_config['open_exchange']) 
+        {
+        ?>
             <span class="input-group-addon" id='rate_simbol'>￥0</span>
+        <?php
+        }
+        ?>
         </div>
 
     </div>
 </div>
-<?php
-}
-else
-{
-    ?>
-    <div class="form-group" id="mul_amount">
-        <label class="col-sm-1 control-label no-padding-right"><?php echo $item_customization_value['title'];?></label>
-        <div class="col-xs-6 col-sm-6">
-            <input type="text" class="form-controller col-xs-12" name="amount" id="amount" placeholder="金额" required>
-        </div>
 
-    </div>
-
-<?php
-    }
-?>
 <script type="text/javascript">
-function init_amount(item_info)
+function init_amount()
 {
     $('#amount').val(item_info['amount']);
 }
 
-function init_coin_type(item_info)
-{
-    $('#coin_type').val(item_info['currency']).prop('selected',true);
-    $('#coin_type').trigger('change');
-    $('#coin_type').trigger('chosen:updated');
-}
-
-function init_amount_module(item_info)
+function init_amount_module()
 {
     init_amount(item_info);
     init_coin_type(item_info);
     $('#amount').trigger('change');
 }
 
-function get_typed_currency(is_init)
-{
-     $.ajax({
-        url:__BASE + 'items/get_typed_currency',
-        dataType:'json',
-        method:'GET',
-        success:function(data){
-            var _h = '';
-            for(var item in data)
-            {
-                typed_currency[item] = JSON.parse(data[item]);
-                _h += '<option data-symbol="' + icon_dic[item] + '" data-rate="' + typed_currency[item]['value'] + '" value="' + item + '">' + simbol_dic[item] + '</option>';
-            }
-            $('#coin_type').append(_h);
-            $('#coin_type').trigger('chosen:updated');
-            if(is_init != 0)
-            {
-                init_amount_module(item_info);
-            }
-        },
-        error:function(a,b,c){
-          
-        }
+$(document).ready(function(){
+    $('#amount').change(function(){
+        var coin_type_id = $('#coin_type').val();
+        var selected_coin = $('#coin_type option:selected');
+        var coin_symbol = selected_coin.data('symbol');
+        var coin_rate = selected_coin.data('rate');
+        $('#coin_simbol').text(coin_symbol);
+        var _amount = $('#amount').val();
+        $('#rate_simbol').text( '￥' + Math.round(_amount*coin_rate)/100 );
     });
-}
-    $(document).ready(function(){
-        if(__config['open_exchange']){
-            // get_currency();
-            get_typed_currency(PAGE_TYPE);
-        }
 
-        $('#coin_type').change(function(){
-            var coin_type_id = $('#coin_type').val();
-            var selected_coin = $('#coin_type option:selected');
-            var coin_symbol = selected_coin.data('symbol');
-            var coin_rate = selected_coin.data('rate');
-            $('#coin_simbol').text(coin_symbol);
-            var _amount = $('#amount').val();
-            $('#rate_simbol').text('￥' + Math.round(_amount*coin_rate)/100);
-            if(coin_type_id != 'cny')
-            {
-                console.log(coin_type_id);
-                console.log();
-                if(typed_currency[coin_type_id]['type'] == 0)
-                {
-                    $('#rate_type').text('现钞卖出价');
-                }
-                if(typed_currency[coin_type_id]['type'] == 2)
-                {
-                    $('#rate_type').text('现汇卖出价');
-                }
-                $('#rate_amount').text(Math.round(coin_rate*10000)/1000000);
-            }
-            else
-            {
-                 $('#rate_type').text('现钞卖出价');
-                 $('#rate_amount').text('1.0');
-            }
-           
-            $('#amount').trigger('change');
-            $('#amount').trigger('change:updated');
-          
-        });
-
-        $('#amount').change(function(){
-            var coin_type_id = $('#coin_type').val();
-            var selected_coin = $('#coin_type option:selected');
-            var coin_symbol = selected_coin.data('symbol');
-            var coin_rate = selected_coin.data('rate');
-            $('#coin_simbol').text(coin_symbol);
-            var _amount = $('#amount').val();
-            $('#rate_simbol').text( '￥' + Math.round(_amount*coin_rate)/100 );
-        });
-    });
+    if(PAGE_TYPE != 0)
+    {
+        init_amount();
+    }
+});
 
 
 
--- application/views/module/items/item_custom_types.php
+++ application/views/module/items/item_custom_types.php
@@ -1,7 +1,7 @@
 <div class="form-group">
 <label class="col-sm-1 control-label no-padding-right"><?php echo $item_customization_value['title'];?></label>
-<div class="col-xs-6 col-sm-6">
-<select class="form-control" id="item_type" name="type" data-placeholder="请选择类型">
+<div class="col-xs-3 col-sm-3">
+<select class="form-control  chosen-select tag-input-style" id="item_type" name="type" data-placeholder="请选择类型">
 <option value="0"><?php echo $item_type_dic[0];?></option>
 <?php 
 if(!$company_config['disable_borrow'])
