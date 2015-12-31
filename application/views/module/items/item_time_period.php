a193077027b918bbbc2b59161b7e8883bdba75e5
瞿庆义 <qingyi.qu@cloudbaoxiao.com>
2015-12-31 12:42:13 +0800

    时间段字段添加开始时间，并将时间段时间和时间字段值设置一致

--- application/views/module/items/item_time_period.php
+++ application/views/module/items/item_time_period.php
@@ -1,4 +1,17 @@
 <?php echo get_html_container($item_customization_value,'html_item_customization_period',true); ?>
+
+<div class="form-group customization_form" data-value='<?php echo htmlspecialchars(json_encode($item_customization_value));?>'>
+	<label class="col-sm-1 control-label no-padding-right" id='time_label'><?php echo $item_customization_value['extra']['title_start'];?></label>
+	<div class="col-xs-4 col-sm-4">
+		<div class="input-group">
+			<input id="start_dt" name="start_dt" type="text" class="form-control date-timepicker" data-id="<?php echo $item_customization_value['id'];?>" 
+				   value="" />
+			<span class="input-group-addon">
+				<i class="fa fa-clock-o bigger-110"></i>
+			</span>
+		</div>
+	</div>
+</div>
 <div class="form-group customization_form" data-value='<?php echo htmlspecialchars(json_encode($item_customization_value));?>'>
 	<label class="col-sm-1 control-label no-padding-right"><?php echo $item_customization_value['title'];?></label>
 	<div class="col-xs-4 col-sm-4">
@@ -26,6 +39,7 @@
     ?>
 </div>
 <script type="text/javascript">
+var is_set_time = false;
 function stringToDate(str)
 {
 	str = str.replace(/-/g,"/");
@@ -34,6 +48,7 @@ function stringToDate(str)
 }
 function init_time_period_module()
 {
+	$('#start_dt').val(item_info['dt']);
 	$('#end_dt').val(item_info['end_dt']);
 	set_day_average();
 }
@@ -41,11 +56,28 @@ function init_time_period_module()
 function set_day_average()
 {
 	var amount = $('#amount').val();
-	var start_day = $('#date-timepicker1').val();
+	var start_day = $('#start_dt').val();
 	var start_date = stringToDate(start_day);
 	var end_day = $('#end_dt').val();
 	var end_date = stringToDate(end_day);
 	var days = Math.ceil((end_date - start_date)/(1000*24*3600));
+	if(!is_set_time)
+	{
+		if(start_day)
+		{
+			$('#end_dt').val(start_day);
+			$('#date-timepicker1').val(start_day);
+			is_set_time = true;
+			show_notify('请选择开始或者结束时间');
+		}
+		if(end_day)
+		{
+			$('#start_dt').val(end_day);
+			$('#date-timepicker1').val(end_day);
+			is_set_time = true;
+			show_notify('请选择开始或者结束时间');
+		}
+	}
 	if(days > 0)
 	{
 		$('#days').text(days);
@@ -106,14 +138,18 @@ $('.date-timepicker').each(function(){
 	});
 });
 
+$('#start_dt').on('change',function(){
+	$('#date-timepicker1').val($('#start_dt').val());
+});
+
 $('#amount').on('change',function(){
 	set_day_average();
 });
 
 
 $(document).ready(function(){
-	var item_customization_value = $('#html_item_customization_period').data('value');
-	$('#time_label').text(item_customization_value['extra']['title_start']);
+//	var item_customization_value = $('#html_item_customization_period').data('value');
+//	$('#time_label').text(item_customization_value['extra']['title_start']);
 	//init_timepicker();
 	if(PAGE_TYPE != 0)
 	{
