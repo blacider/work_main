7147af39f0a3a7cc4600ba7e063039864207d22c
niurenpeng <niurenpeng@niurenpeng.com>
2015-12-31 15:24:34 +0800

    Tab -> 四个空格

--- static/js/jqgrid_choseall.js
+++ static/js/jqgrid_choseall.js
@@ -2,11 +2,11 @@
 // grid 为文本型
 // 返回 ids 数组
 function jqgrid_choseall_plus(grid) {
-	var $grid = $(grid);
-	var result = new Array();
-	var data = jQuery(grid).jqGrid('getGridParam').data;
-	for (var i = data.length - 1; i >= 0; i--) {
-		result.push(data[i].id);
-	};
-	return result;
+    var $grid = $(grid);
+    var result = new Array();
+    var data = jQuery(grid).jqGrid('getGridParam').data;
+    for (var i = data.length - 1; i >= 0; i--) {
+        result.push(data[i].id);
+    };
+    return result;
 }
