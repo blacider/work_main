8363614afcb04ee76562fcc5222d356ba5e3fcf6
niurenpeng <niurenpeng@niurenpeng.com>
2016-01-06 13:47:59 +0800

    重写 choseall，使能全选 filter 之后的全部数据

--- static/js/jqgrid_choseall.js
+++ static/js/jqgrid_choseall.js
@@ -1,10 +1,22 @@
 //获取当前显示的表格的所有数据,
 // grid 为文本型
 // 返回 ids 数组
+
+var oldFrom = $.jgrid.from,
+    lastSelected;
+
+$.jgrid.from = function (source, initalQuery) {
+    var result = oldFrom.call(this, source, initalQuery),old_select = result.select;
+    result.select = function (f) {
+        lastSelected = old_select.call(this, f);
+        return lastSelected;
+    };
+    return result;
+};
+
 function jqgrid_choseall_plus(grid) {
-    var $grid = $(grid);
     var result = new Array();
-    var data = jQuery(grid).jqGrid('getGridParam').data;
+    var data = $(grid).jqGrid('getGridParam', 'lastSelected');
     for (var i = data.length - 1; i >= 0; i--) {
         result.push(data[i].id);
     };
