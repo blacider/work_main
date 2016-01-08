68cb4a633fa7fd494428ff4a0248f0ad824d3a8e
niurenpeng <niurenpeng@niurenpeng.com>
2016-01-08 12:37:59 +0800

    银行列表修正

--- application/views/reports/template_new.php
+++ application/views/reports/template_new.php
@@ -446,7 +446,7 @@ foreach($items as $i){
                                         <option value='南京市商业银行'>南京市商业银行</option>
                                         <option value='三门峡市城市信用社'>三门峡市城市信用社</option>
                                         <option value='沈阳市商业银行'>沈阳市商业银行</option>
-                                        <option value='西宁市农商银行'>西宁市农商银行</option>
+                                        <option value='西宁市农商银行'>西宁农商银行</option>
                                         <option value='浙江省农村信用社联合社'>浙江省农村信用社联合社</option>
                                         <option value='星展银行'>星展银行</option>
                                         <option value='绍兴商业银行'>绍兴商业银行</option>
@@ -557,7 +557,7 @@ foreach($items as $i){
                                         <option value='靖江市长江城市信用社'>靖江市长江城市信用社</option>
                                         <option value='郑州商业银行'>郑州商业银行</option>
                                         <option value='集友银行'>集友银行</option>
-                                        <option value='中江市农村信用社'>中江农村信用社</option>
+                                        <option value='中江市农村信用社'>中江市农村信用社</option>
                                     </select>
                                 </div>
                             </div>
--- application/views/user/profile.php
+++ application/views/user/profile.php
@@ -642,7 +642,7 @@ if(in_array($profile['admin'],[1,3,4])){
                                         <option value='南京市商业银行'>南京市商业银行</option>
                                         <option value='三门峡市城市信用社'>三门峡市城市信用社</option>
                                         <option value='沈阳市商业银行'>沈阳市商业银行</option>
-                                        <option value='西宁市农商银行'>西宁市农商银行</option>
+                                        <option value='西宁市农商银行'>西宁农商银行</option>
                                         <option value='浙江省农村信用社联合社'>浙江省农村信用社联合社</option>
                                         <option value='星展银行'>星展银行</option>
                                         <option value='绍兴商业银行'>绍兴商业银行</option>
@@ -753,7 +753,7 @@ if(in_array($profile['admin'],[1,3,4])){
                                         <option value='靖江市长江城市信用社'>靖江市长江城市信用社</option>
                                         <option value='郑州商业银行'>郑州商业银行</option>
                                         <option value='集友银行'>集友银行</option>
-                                        <option value='中江市农村信用社'>中江农村信用社</option>
+                                        <option value='中江市农村信用社'>中江市农村信用社</option>
                                     </select>
                                 </div>
                             </div>
