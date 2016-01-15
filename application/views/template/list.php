9943755e294a72fb6c49f2b116f411ba2639ba66
zhangjie <jie.zhang@yunbaoxiao.com>
2016-01-15 18:17:35 +0800

    add template

---
+++ application/views/template/list.php
@@ -0,0 +1,83 @@
+<script src="/app/libs/angular/angular.js"></script>
+<link rel="stylesheet" href="/static/css/mod/template/list.css"/>
+<div class="mod mod-reim-template" ng-app="reimApp">
+	<div class="page-content-area" ng-controller="templateController">
+		<div class="header">
+			<a href="javascript:void(0)" class="btn-add">
+				
+			</a>
+		</div>
+		<div class="paper">
+			<div class="paper-header"> </div>
+			<div class="paper-content">
+				<div class="title">内容设置</div>
+				<div class="field-group">
+					<h4 class="field-group-title">差旅详情
+					<p class="buttons">
+						<span class="button btn-trash"></span>
+						<span class="button btn-edit"></span>
+						<span class="button btn-drag"></span>
+					</p>
+					</h4>
+					<h4 class="field-group-label">
+					字段组
+					</h4>
+					<div class="field-group-table bordered">
+						<table>
+							<thead>
+								<tr>
+									<th>起始地点</th>
+									<th>起始地点</th>
+									<th>起始地点</th>
+									<th>起始地点</th>
+								</tr>
+							</thead>
+							<tbody>
+								<tr>
+									<td>起始地点</td>
+									<td>起始地点</td>
+									<td>起始地点</td>
+									<td>起始地点</td>
+								</tr>
+							</tbody>
+						</table>
+					</div>
+				</div>
+				<div class="field-group">
+					<h4 class="field-group-title">差旅详情
+					<p class="buttons">
+						<span class="button btn-trash"></span>
+						<span class="button btn-edit"></span>
+						<span class="button btn-drag"></span>
+					</p>
+					</h4>
+					<h4 class="field-group-label">
+					字段组
+					</h4>
+					<div class="field-group-table bordered">
+						<table>
+							<thead>
+								<tr>
+									<th>起始地点</th>
+									<th>起始地点</th>
+									<th>起始地点</th>
+									<th>起始地点</th>
+								</tr>
+							</thead>
+							<tbody>
+								<tr>
+									<td>起始地点</td>
+									<td>起始地点</td>
+									<td>起始地点</td>
+									<td>起始地点</td>
+								</tr>
+							</tbody>
+						</table>
+					</div>
+				</div>
+			</div>
+			<div class="paper-footer"></div>
+		</div>
+	</div>
+</div>
+<script src="/static/js/mod/template/list.js"></script>
