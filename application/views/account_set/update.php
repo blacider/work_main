404eb33ffa814f3c88ce946064d4c886026d6e5d
niurenpeng <niurenpeng@niurenpeng.com>
2016-02-04 23:43:10 +0800

    类目名称限制为10字符

--- application/views/account_set/update.php
+++ application/views/account_set/update.php
@@ -325,7 +325,7 @@ if($last_error) {
     <div class="modal-body">
         <div class="form-group">
             <label class="col-sm-2 col-xl-2">名称</label>
-            <input type="text" maxlength="30" id='update_cate_name' name='name' data-placeholder="请输入名称"></div>
+            <input type="text" maxlength="10" id='update_cate_name' name='name' data-placeholder="请输入名称"></div>
         <div class="form-group">
             <label class="col-sm-2 control-label no-padding-rigtht">说明</label>
                             
@@ -415,7 +415,7 @@ if($last_error) {
     <div class="modal-body">
         <div class="form-group">
             <label class="col-sm-2 col-xl-2">名称</label>
-            <input type="text" maxlength="30" id='create_cate_name' name='name' data-placeholder="请输入名称"></div>
+            <input type="text" maxlength="10" id='create_cate_name' name='name' data-placeholder="请输入名称"></div>
         <div class="form-group">
             <label class="col-sm-2 control-label no-padding-rigtht">说明</label>
                             
