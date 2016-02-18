28850f3967a252d8b5a882700441f060bd51e0c1
zhangjie <jie.zhang@yunbaoxiao.com>
2016-02-18 18:22:21 +0800

    HTML 格式美化

--- application/views/user/login.new.php
+++ application/views/user/login.new.php
@@ -1,123 +1,122 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title> 云报销 - 对报销的怨言，到此为止。</title>
-    <meta name="viewport" content="width=device-width,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no" /> 
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <link rel="shortcut icon" href="favicon.ico" />
-    <meta name="baidu-site-verification" content="74glnE71aV" />
-    <meta name="description" content="最好用的财务报销审批系统。对报销的怨言，到此为止。">
-    <meta name="keywords" content="云报销,报销,云报销,报销,财务审批,审批,财务,办公,发票,贴发票,贴票,成本,金额">
-    <meta name="format-detection" content="telephone=no" />
-    <meta property="wb:webmaster" content="a239efc5a51c65f0" />
-    <link rel="stylesheet" href="/static/css/reset.css">
-    <link rel="stylesheet" href="/static/css/mod/login/index.css">
-    <link rel="stylesheet" href="/static/css/mod/login/common.css">
-</head>
-<body>
-    <div id="header">
-        <div class="wrap-inner">
-            <a class="logo" href=""><img src="/static/img/mod/login/logo.png" alt=""></a>
-            <ul class="menu">
-                <li><a href="/">Blog</a></li>
-                <li><a href="/">直接体验</a></li>
-                <li><a href="/">登录</a></li>
-                <li class="register"><a href="/">企业注册</a></li>
-            </ul>
-        </div>
-    </div>
-    <div id="main">
-        <p class="slogan">
-            <h2 style="display: none">对报销的怨言，到此为止。</h2>
-            <img src="/static/img/mod/login/slogan.png" alt="">
-        </p>
-        <div class="login-box">
-            <input class="account" type="text" value="" placeholder="邮箱／手机号码">
-            <input class="login" type="button" value="创建公司">
-        </div>
-        
-    </div>
-    <div class="block2 block">
-        <div class="main-block">
-            <div class="block2-left">
-                <div class="block2-left-img"></div>
-                <div class="block2-left-text">
-                  <p>专注解决企业的</p>
-                  <p>财务报销与审批问题</p>
-                </div>
-            </div>
-            <div class="block2-right">
-                <div class="block2-right-content block2-right-1">
-                    <div class="block2-right-content-img1 icon"></div>
-                    <div class="block2-right-content-text">
-                        <p class="block2-right-text1">多平台支持</p>
-                        <p class="block2-right-text2">Android, iOS, Web端</p>
-                    </div>
-                </div>
-                <div class="block2-right-content block2-right-2">
-                    <div class="block2-right-content-img2 icon"></div>
-                    <div class="block2-right-content-text">
-                        <p class="block2-right-text1">云平台存储</p>
-                        <p class="block2-right-text2">部署快捷、更加安全</p>
-                    </div>
-                </div>
-                <div class="block2-right-content block2-right-3">
-                    <div class="block2-right-content-img3 icon"></div>
-                    <div class="block2-right-content-text">
-                        <p class="block2-right-text1">财务软件对接</p>
-                        <p class="block2-right-text2">支持与各类ERP、网上银行系统等对接</p>
-                    </div>
-                </div>
+    <head>
+        <meta charset="UTF-8">
+        <title> 云报销 - 对报销的怨言，到此为止。</title>
+        <meta name="viewport" content="width=device-width,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <link rel="shortcut icon" href="favicon.ico" />
+        <meta name="baidu-site-verification" content="74glnE71aV" />
+        <meta name="description" content="最好用的财务报销审批系统。对报销的怨言，到此为止。">
+        <meta name="keywords" content="云报销,报销,云报销,报销,财务审批,审批,财务,办公,发票,贴发票,贴票,成本,金额">
+        <meta name="format-detection" content="telephone=no" />
+        <meta property="wb:webmaster" content="a239efc5a51c65f0" />
+        <link rel="stylesheet" href="/static/css/reset.css">
+        <link rel="stylesheet" href="/static/css/mod/login/index.css">
+        <link rel="stylesheet" href="/static/css/mod/login/common.css">
+    </head>
+    <body>
+        <div id="header">
+            <div class="wrap-inner">
+                <a class="logo" href=""><img src="/static/img/mod/login/logo.png" alt=""></a>
+                <ul class="menu">
+                    <li><a href="/">Blog</a></li>
+                    <li><a href="/">直接体验</a></li>
+                    <li><a href="/">登录</a></li>
+                    <li class="register"><a href="/">企业注册</a></li>
+                </ul>
             </div>
         </div>
-    </div>
-    <div class="block3">
-        <div class="main-block">
-            <div class="block3-title"><h1>全新的报销体验</h1></div>
-            <div class="block3-main">
-                <div class="block3-content block3-content1">
-                  <div id="white-phone" class="block3-content-img"></div>
-                  <div class="block3-text">
-                    <p class="block3-content-text1">随时，随地</p>
-                    <p class="block3-content-text2">快速添加消费<br>
-                    随时提交审批<br>公司政策提醒</p>
-                  </div>
-                  
+        <div id="main">
+            <p class="slogan">
+                <h2 style="display: none">对报销的怨言，到此为止。</h2>
+                <img src="/static/img/mod/login/slogan.png" alt="">
+            </p>
+            <div class="login-box">
+                <input class="account" type="text" value="" placeholder="邮箱／手机号码">
+                <input class="login" type="button" value="创建公司">
+            </div>
+            
+        </div>
+        <div class="block2 block">
+            <div class="main-block">
+                <div class="block2-left">
+                    <div class="block2-left-img"></div>
+                    <div class="block2-left-text">
+                        <p>专注解决企业的</p>
+                        <p>财务报销与审批问题</p>
+                    </div>
                 </div>
-                <div class="block3-content block3-content2">
-                  <div id="black-phone" class="block3-content-img"></div>
-                  <div class="block3-text">
-                    <p class="block3-content-text1">实时，简捷</p>
-                    <p class="block3-content-text2">便捷归档消费记录<br>
-                    实时查询报销进度<br>规范公司财务流程</p>
-                  </div>
-                </div>
-                <div class="block3-content block3-content3">
-                  <div id="white-pc" class="block3-content-img"></div>
-                  <div class="block3-text">
-                    <p class="block3-content-text1">清晰，直观</p>
-                    <p class="block3-content-text2">掌握公司分类支出<br>
-                    定制多种统计图表<br>迅速定位每笔消费</p>
-                  </div>
+                <div class="block2-right">
+                    <div class="block2-right-content block2-right-1">
+                        <div class="block2-right-content-img1 icon"></div>
+                        <div class="block2-right-content-text">
+                            <p class="block2-right-text1">多平台支持</p>
+                            <p class="block2-right-text2">Android, iOS, Web端</p>
+                        </div>
+                    </div>
+                    <div class="block2-right-content block2-right-2">
+                        <div class="block2-right-content-img2 icon"></div>
+                        <div class="block2-right-content-text">
+                            <p class="block2-right-text1">云平台存储</p>
+                            <p class="block2-right-text2">部署快捷、更加安全</p>
+                        </div>
+                    </div>
+                    <div class="block2-right-content block2-right-3">
+                        <div class="block2-right-content-img3 icon"></div>
+                        <div class="block2-right-content-text">
+                            <p class="block2-right-text1">财务软件对接</p>
+                            <p class="block2-right-text2">支持与各类ERP、网上银行系统等对接</p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
-      </div>
-      <div class="block5">
+        <div class="block3">
+            <div class="main-block">
+                <div class="block3-title"><h1>全新的报销体验</h1></div>
+                <div class="block3-main">
+                    <div class="block3-content block3-content1">
+                        <div id="white-phone" class="block3-content-img"></div>
+                        <div class="block3-text">
+                            <p class="block3-content-text1">随时，随地</p>
+                            <p class="block3-content-text2">快速添加消费<br>
+                            随时提交审批<br>公司政策提醒</p>
+                        </div>
+                        
+                    </div>
+                    <div class="block3-content block3-content2">
+                        <div id="black-phone" class="block3-content-img"></div>
+                        <div class="block3-text">
+                            <p class="block3-content-text1">实时，简捷</p>
+                            <p class="block3-content-text2">便捷归档消费记录<br>
+                            实时查询报销进度<br>规范公司财务流程</p>
+                        </div>
+                    </div>
+                    <div class="block3-content block3-content3">
+                        <div id="white-pc" class="block3-content-img"></div>
+                        <div class="block3-text">
+                            <p class="block3-content-text1">清晰，直观</p>
+                            <p class="block3-content-text2">掌握公司分类支出<br>
+                            定制多种统计图表<br>迅速定位每笔消费</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="block5">
             <div class="main-block">
                 <div class="block5-left">
                     <div class="block5-left-text block5-text">
-                      <p>这显然是一个用移动互联网的基因做出来的，专注解决报销审批这个问题的服务——用「报销」这个需求，去搭建公司内部的资金与工作/项目的关系链。</p>
-
-                      <p>员工们实时记账，提交给上司审批，同时转给财务执行。每个公司、每个项目相当于一个组群，由不同员工、项目灵活组成。而如果员工离职，则退出之前的组群，加入新公司的组群。做产品的2C心态，也使得B端用户有着很好的体验。</p>
+                        <p>这显然是一个用移动互联网的基因做出来的，专注解决报销审批这个问题的服务——用「报销」这个需求，去搭建公司内部的资金与工作/项目的关系链。</p>
+                        <p>员工们实时记账，提交给上司审批，同时转给财务执行。每个公司、每个项目相当于一个组群，由不同员工、项目灵活组成。而如果员工离职，则退出之前的组群，加入新公司的组群。做产品的2C心态，也使得B端用户有着很好的体验。</p>
                     </div>
                     <div id="triangle-topleft"></div>
                     <div class="block5-contact">
                         <div class="block5-img block5-img-1"></div>
                         <div class="block5-contact-text">
                             <p>dvdv</p>
-                          <p>虎嗅网·联合创始人</p>
+                            <p>虎嗅网·联合创始人</p>
                         </div>
                     </div>
                 </div>
@@ -131,29 +130,29 @@
                     </div>
                     <div id="triangle-bottomright"></div>
                     <div class="block5-right-text block5-text">
-                      <p>做为一个小企业主，能找到一款有着亲民气质的报销软件实在是太难了，如何把专业的流程进行更贴近人性化的设计，如何更趁手，如何让软件适应我们用户而不是我们去适应软件。</p><p>很多个「如何」，直到用了云报销才知道了答案。更重要的是，作为用户，能感受到隐藏在产品中的幽默感和贴心，这一切都让人对这个产品的好感值急升。</p>
-                      <p>
-                       </p>
+                        <p>做为一个小企业主，能找到一款有着亲民气质的报销软件实在是太难了，如何把专业的流程进行更贴近人性化的设计，如何更趁手，如何让软件适应我们用户而不是我们去适应软件。</p><p>很多个「如何」，直到用了云报销才知道了答案。更重要的是，作为用户，能感受到隐藏在产品中的幽默感和贴心，这一切都让人对这个产品的好感值急升。</p>
+                        <p>
+                        </p>
                     </div>
                 </div>
             </div>
         </div>
         <div class="footer">
             <div class="main-block">
-              <div class="copyright">
-                <p class="footer-text">©2016 如数科技有限公司 / All Rights Reserved</p>
-              </div>
-              <div class="footer-link">
-                <ul>
-                    <li><a href="contact.html">关于我们</a></li>
-                    <li><a href="help.html">帮助中心</a></li>
-                    <li><a href="joinus.html">加入我们</a></li>
-                </ul>
-              </div>
-              <div class="record">
-                <p class="footer-text">京ICP备14046885号-2</p>
-              </div>
+                <div class="copyright">
+                    <p class="footer-text">©2016 如数科技有限公司 / All Rights Reserved</p>
+                </div>
+                <div class="footer-link">
+                    <ul>
+                        <li><a href="contact.html">关于我们</a></li>
+                        <li><a href="help.html">帮助中心</a></li>
+                        <li><a href="joinus.html">加入我们</a></li>
+                    </ul>
+                </div>
+                <div class="record">
+                    <p class="footer-text">京ICP备14046885号-2</p>
+                </div>
             </div>
-          </div>
-</body>
+        </div>
+    </body>
 </html>
