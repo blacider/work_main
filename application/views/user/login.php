e7837cc6fc85ffe44aa38e8422a402d7feb9a7a5
zhangjie <jie.zhang@yunbaoxiao.com>
2016-02-23 17:02:55 +0800

    加入美洽聊天插件

--- application/views/user/login.php
+++ application/views/user/login.php
@@ -206,6 +206,22 @@
       s.parentNode.insertBefore(hm, s);
     })();
     </script>
+
+    <!-- 美洽 -->
+    <script type='text/javascript'>
+        (function(m, ei, q, i, a, j, s) {
+            m[a] = m[a] || function() {
+                (m[a].a = m[a].a || []).push(arguments)
+            };
+            j = ei.createElement(q),
+                s = ei.getElementsByTagName(q)[0];
+            j.async = true;
+            j.charset = 'UTF-8';
+            j.src = i + '?v=' + new Date().getUTCDate();
+            s.parentNode.insertBefore(j, s);
+        })(window, document, 'script', '//eco-api.meiqia.com/dist/meiqia.js', '_MEIQIA');
+        _MEIQIA('entId', 7359);
+    </script>
   </div>
 </body>
 </html>
