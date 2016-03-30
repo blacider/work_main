96115605a527dd54eb76e2b2b45559443d9d90ba
zhangjie <jie.zhang@yunbaoxiao.com>
2016-03-30 18:20:31 +0800

    添加登录成功页面

---
+++ application/views/user/reg_successful.php
@@ -0,0 +1,136 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <script>
+            var _hmt = _hmt || [];
+        </script>
+        <script type='text/javascript'>
+        var _vds = _vds || [];
+        window._vds = _vds;
+        (function() {
+            _vds.push(['setAccountId', 'c51af5ab801b549d51103ce73e4ccd6f']);
+            (function() {
+                var vds = document.createElement('script');
+                vds.type = 'text/javascript';
+                vds.async = true;
+                vds.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'dn-growing.qbox.me/vds.js';
+                var s = document.getElementsByTagName('script')[0];
+                s.parentNode.insertBefore(vds, s);
+            })();
+        })();
+        </script>
+        <title> 云报销 - 对报销的怨言，到此为止。</title>
+        <meta name="viewport" content="width=device-width,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <link rel="shortcut icon" href="favicon.ico" />
+        <meta name="baidu-site-verification" content="74glnE71aV" />
+        <meta name="description" content="最好用的财务报销审批系统。对报销的怨言，到此为止。">
+        <meta name="keywords" content="云报销,报销,云报销,报销,财务审批,审批,财务,办公,发票,贴发票,贴票,成本,金额">
+        <meta name="format-detection" content="telephone=no" />
+        <meta property="wb:webmaster" content="a239efc5a51c65f0" />
+        <link rel="stylesheet" href="/static/css/reset.css">
+        <script src="/static/js/jquery.min.js"></script>
+        <!--[if lt IE 9]>
+        <script src="static/js/respond.min.js"></script>
+        <script src="/static/js/mod/login/placeholder.js"></script>
+        <link rel="stylesheet" type="text/css" href="static/css/ie.css">
+        <![endif]-->
+        <!--[if gte IE 9]>
+        <link rel="stylesheet" type="text/css" href="static/css/notie.css">
+        <![endif]-->
+        <!--[if !IE]><!-->
+        <link rel="stylesheet" type="text/css" href="static/css/notie.css">
+        <!--<![endif]-->
+        <style>
+        #main {
+        	text-align: center;
+        	max-width: 480px;
+        	margin: 100px auto;
+        }
+        #main h1 {
+        	color: #ff575b;
+        }
+        #main .thanks {
+        	text-indent: 48px;
+        	font-size: 14px;
+        	color: #666;
+        	display: inline-block;
+		    margin-bottom: 15px;
+		    text-align: left;
+		    vertical-align: top;
+		    padding: 0 20px;
+        }
+        #main .thanks span {
+			display: block;
+			margin: 5px 0;
+			vertical-align: baseline;
+			line-height: 15px;
+        }
+        #main .thanks span a {
+			text-indent: 48px;
+			text-decoration: none;
+			line-height: 20px;
+			color: #666;
+        }
+        img.logo {
+        	border-radius: 100%;
+        	width: 80px;
+        }
+
+        </style>
+    </head>
+    <body>
+		<div id="main">
+			<a href="/"><img class="logo" src="/static/logo.png" alt=""></a>
+			<h1>报销君，您已经注册完成！</h1>
+			<p>
+				<a href="/">点击进入站点</a>
+				<br>
+				<br>
+			</p>
+			<p class="thanks">
+			<span>
+		       	<a>
+		       		感谢试用 『云报销』，试用期间您将免费使用产品的部分功能。
+				如果您希望进一步了解 云报销，请与我们的客服联系。
+				云报销，对报销的怨言到此为止！
+		       	</a>
+			</span>
+			</p>
+			<div class="code">
+				<p>请使用手机扫码下载 云报销手机客户端</p>
+				<img src="/static/img/download.png" style="width:160px;height:160px; margin: 20px">
+			</div>
+		</div>
+        <?php if(!$has_attacker) { ?>
+        <!-- 百度统计 -->
+        <div style="display: none;">
+        <script>
+        var _hmt = _hmt || [];
+        (function() {
+          var hm = document.createElement("script");
+          hm.src = "//hm.baidu.com/hm.js?777289b210223dd4e237d8e2089250fd";
+          var s = document.getElementsByTagName("script")[0]; 
+          s.parentNode.insertBefore(hm, s);
+        })();
+        </script>
+        </div>
+        <?php } ?>
+        <!-- 美洽 -->
+        <script type='text/javascript'>
+            (function(m, ei, q, i, a, j, s) {
+                m[a] = m[a] || function() {
+                    (m[a].a = m[a].a || []).push(arguments)
+                };
+                j = ei.createElement(q),
+                    s = ei.getElementsByTagName(q)[0];
+                j.async = true;
+                j.charset = 'UTF-8';
+                j.src = i + '?v=' + new Date().getUTCDate();
+                s.parentNode.insertBefore(j, s);
+            })(window, document, 'script', '//eco-api.meiqia.com/dist/meiqia.js', '_MEIQIA');
+            _MEIQIA('entId', 7359);
+        </script>
+    </body>
+</html>
