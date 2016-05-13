10536aa2e83d77015172983464277fb4672e503e
zhangjie <jie.zhang@yunbaoxiao.com>
2016-05-13 17:33:19 +0800

    修复消费备注展示不全的BUG

--- application/views/module/items/item_notes.php
+++ application/views/module/items/item_notes.php
@@ -2,7 +2,7 @@
 <div class="form-group customization_form" data-value='<?php echo htmlspecialchars(json_encode($item_customization_value));?>'>
 <label class="col-sm-1 control-label no-padding-right"><?php echo $item_customization_value['title'];?></label>
 <div class="col-xs-6 col-sm-6">
-<input name="note" id="note" type="text" data-title="<?php echo $item_customization_value['title'];?>" data-id="<?php echo $item_customization_value['id'];?>" class="col-xs-12 col-sm-12 form-controller default_custom need_check" placeholder="<?php echo $item_customization_value['description'];?>">
+<textarea class="form-control" name="note" id="note" style="min-height: 80px;" data-title="<?php echo $item_customization_value['title'];?>" data-id="<?php echo $item_customization_value['id'];?>" class="col-xs-12 col-sm-12 form-controller default_custom need_check" placeholder="<?php echo $item_customization_value['description'];?>" ></textarea> 
 </div>
 
 </div>
