bc2570dc8ffc3bf406fa4f4fb9978e380948eda5
George Xie <georgexsh@gmail.com>
2016-05-16 13:57:14 +0800

    rm /pub/wx

--- application/controllers/Pub.php
+++ application/controllers/Pub.php
@@ -4,124 +4,12 @@
 class Pub extends REIM_Controller {
     public function __construct() {
         parent::__construct();
-        $this->load->model('tags_model', 'tags');
         $this->load->model('app_model');
         $this->load->model('group_model', 'groups');
         $this->load->model('user_model', 'users');
-        $this->load->model('report_model', 'reports');
         $this->load->library('user_agent');
 
     }
-
-    public function invite($gid = 0, $uid = 0){
-        if(0 === $gid || 0 === $uid){
-            die("「参数错误」");
-        }
-
-
-    }
-
-
-    public function wx(){
-        $TOKEN = 'Rushu0915';
-
-        //
-        //signature=d0d9c77994e9a86244595dfd3e96d3f4a4e96233
-        //timestamp=1432457431
-        //nonce=1988866622&
-        //encrypt_type=aes&
-        //msg_signature=bfb1727cd0bb727ef633a9ff2d2321c0c91e810a
-        //
-        $signature = $_GET["signature"];
-        $timestamp = $_GET["timestamp"];
-        $nonce = $_GET["nonce"];  
-
-        $http_method = strtolower($this->input->server('REQUEST_METHOD'));
-        if($http_method == "get") {
-            $echostr = $_GET["echostr"];  
-            $token = $TOKEN;
-            $tmpArr = array($token, $timestamp, $nonce);
-            sort($tmpArr, SORT_STRING);
-            $tmpStr = implode($tmpArr);
-            $tmpStr = sha1($tmpStr);
-            if( $tmpStr == $signature ){
-                die($echostr);
-            }else{
-                echo "";
-            }
-        } else {
-            $encrypt_type = $this->input->get('encrypt_type');
-            $msg_signature = $this->input->get('msg_signature');
-
-            $params = array(
-                'token' => $TOKEN
-                ,'encodingAesKey' => 'Sw6N3Bb7a7mGbUjpn7w1wc3TK8ZbjlbtVEsvJ89kB5A'
-                ,'appId' => 'wx068349d5d3a73855'
-            );
-            $this->load->library('wxlib', $params);
-            $msg = file_get_contents("php://input");
-
-            log_message("debug", "WX Msg: " . $msg);
-            log_message("debug", "WX Msg: " . $msg_signature);
-            log_message("debug", "WX ts: " . $timestamp);
-            log_message("debug", "WX nonce: " . $nonce);
-            $sMsg = '';
-            $errCode = $this->wxlib->DecryptMsg($msg_signature, $timestamp, $nonce, $msg, $sMsg);
-            log_message("debug", "error:" . $errCode);
-            //$errCode = $pc->decryptMsg($msg_sign, $timeStamp, $nonce, $from_xml, $msg);
-            if ($errCode == 0) {
-                log_message("debug", "Get Message From WX:" . $sMsg);
-                /*
-                 * <xml><ToUserName><![CDATA[gh_3cc13bf56bfa]]></ToUserName>
-                 * <FromUserName><![CDATA[oh_4Ttwus34wUuNlDRBbCsEK3rVE]]></FromUserName>
-                 * <CreateTime>1432459398</CreateTime>
-                 * <MsgType><![CDATA[text]]></MsgType>
-                 * <Content><![CDATA[x]]></Content>
-                 * <MsgId>6152366267464858206</MsgId>
-                 * </xml>
-                 */
-                $xml_msg = new DOMDocument();
-                $xml_msg->loadXML($sMsg);
-                $msg_type = $xml_msg->getElementsByTagName('MsgType')->item(0)->nodeValue;
-                $from = $xml_msg->getElementsByTagName('FromUserName')->item(0)->nodeValue;
-                $to = $xml_msg->getElementsByTagName('ToUserName')->item(0)->nodeValue;
-                $content = $xml_msg->getElementsByTagName('Content')->item(0)->nodeValue;
-                if($msg_type == "text") {
-                    //$result = file_get_contents("http://i.itpk.cn/api.php?question=" . $content);
-                    //$msg = $this->response_text($result, $timestamp, $nonce, $this->wxlib, $from, $to);
-                    //$msg = $this->response_text("您的消息已经被我们的系统自动接收了", $timestamp, $nonce, $this->wxlib, $from, $to);
-                    //log_message("debug", "EMSG:" . $msg);
-                    //die($msg);
-                    // 用户发来了消息
-                } else if ($msg_type == "event") {
-                    // 处理关注和取消关注的事情
-                    $event_type = $xml_msg->getElementsByTagName('Event')->item(0)->nodeValue;
-                    if("subscribe" == $event_type) {
-                    $msg = $this->response_text("咦，又一个被报销折磨哭的人关注了我们...", $timestamp, $nonce, $this->wxlib, $from, $to);
-                    die($msg);
-                    }
-                }
-
-                print($errCode . "\n");
-            }
-
-        }
-    }
-
-    private function response_text($msg, $timeStamp, $nonce, $wxlib, $to, $from){
-        $text = sprintf("<xml><ToUserName><![CDATA[%s]]></ToUserName><FromUserName><![CDATA[%s]]></FromUserName><CreateTime>%s</CreateTime><MsgType><![CDATA[text]]></MsgType><Content><![CDATA[%s]]></Content></xml>", $to, $from, $timeStamp, $msg);
-        log_message("debug", "Return: " . $text);
-        return $text;
-        $encryptMsg = '';
-        $errCode = $wxlib->encryptMsg($text, $timeStamp, $nonce, $encryptMsg);
-        if ($errCode == 0) {
-            log_message("debug", "Encrypt Success");
-            return $encryptMsg;
-        } else {
-            return "";
-        }
-    }
-
 
     public function oauth($params = ''){
         log_message("debug", "Agent:" . strtolower($this->input->user_agent()));
