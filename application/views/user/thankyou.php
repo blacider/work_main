f8f102daa16867c1e782c489ff4837af700183da
George Xie <georgexsh@gmail.com>
2016-05-17 11:00:36 +0800

    rm /thanksyou

--- application/views/user/thankyou.php
+++
@@ -1,7 +0,0 @@
-<div id="headerwrap">
-    <div class="container">
-        <div class="row">
-            <div class="alert alert-success" role="alert">Well done,thank you!</div>
-        </div><!-- /row -->
-    </div><!-- /container -->
-</div><!-- /headerwrap -->
