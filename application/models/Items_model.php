ec6eddf64a6da84d82df37f413b8a9201c879627
George Xie <georgexsh@gmail.com>
2016-05-26 15:41:28 +0800

    use api_* in Items_model

--- application/models/Items_model.php
+++ application/models/Items_model.php
@@ -4,92 +4,46 @@ class Items_Model extends Reim_Model {
 
     public function get_item_type_name()
     {
-        $jwt = $this->session->userdata('jwt');
-        if(!$jwt) return false;
-        $url = $this->get_url('item_type_name');
-        $buf = $this->do_Get($url, $jwt);
-        log_message('debug','item_type_name_url:' . $url);
-        log_message('debug','item_type_name_back:' . $buf);
-        $obj = json_decode($buf, True);
-        return $obj;
+        return $this->api_get('item_type_name');
     }
 
     public function update_item_type_name($type,$name,$description)
     {
-        $jwt = $this->session->userdata('jwt');
-        if(!$jwt) return false;
-        $url = $this->get_url('item_type_name/' . $type);
         $data = array(
             'type' => $type,
             'name' => $name,
             'description' => $description
         );
-        $buf = $this->do_Put($url,$data,$jwt);
-        log_message('debug','update_item_type_name_url:' . $url);
-        log_message('debug','update_item_type_name_data:' . json_encode($data));
-        log_message('debug','update_item_type_name_back:' . $buf);
-        $obj = json_decode($buf, True);
-        return $obj;
+        return $this->api_put('item_type_name/' . $type, $data);
     }
 
     public function get_typed_currency()
     {
-        $jwt = $this->session->userdata('jwt');
-        if(!$jwt) return false;
-        $url = $this->get_url('typed_currency');
-        $buf = $this->do_Get($url, $jwt);
-        log_message('debug','typed_currency_url:' . $url);
-        log_message('debug','typed_currency_back:' . $buf);
-        $obj = json_decode($buf, True);
-        return $obj;
+        return $this->api_get('typed_currency');
     }
 
     public function get_currency()
     {
-        $jwt = $this->session->userdata('jwt');
-        if(!$jwt) return false;
-        $url = $this->get_url('currency');
-        $buf = $this->do_Get($url, $jwt);
-        log_message("debug", $buf);
-        $obj = json_decode($buf, True);
-        return $obj;
+        return $this->api_get('currency');
     }
 
     public function attachment($content,$filename,$mime)
     {
-        log_message('debug','qqy content: ' . $content);
-        $jwt = $this->session->userdata('jwt');
-        if(!$jwt) return false;
-        $url = $this->get_url('attachment');
+        $this->set_curl_timeout(1200);
         $data = array(
             "content" => $this->get_curl_upload_field($content),
             "filename" => $filename,
             "mime" => $mime,
         );
-        // 自定义超时时间20分钟
-        $this->set_curl_timeout(1200);
-        $buf = $this->do_Post($url, $data, $jwt);
-        log_message('debug','attachment_data:' . json_encode($data));
-        log_message('debug','attachment_url:' . json_encode($url));
-        log_message('debug','attachment_back:' . $buf);
-        log_message("debug", $buf);
-        $obj = json_decode($buf, true);
-        return $obj;
+        return $this->api_post('attachment', $data);
     }
 
     public function update_item($id,$opts){
-        $items = array();
         $data = array(
-              "iid" => $id
-              ,"opts" => json_encode($opts)
-             );
-        $jwt = $this->session->userdata('jwt');
-        $url = $this->get_url('update_item');
-        $buf = $this->do_Post($url, $data, $jwt);
-        $obj = json_decode($buf, true);
-        log_message('debug','update_item_data:'. json_encode($data));
-        log_message('debug','update_item_back:'.$buf);
-        return $obj;
+            "iid" => $id,
+            "opts" => json_encode($opts)
+        );
+        return $this->api_post('update_item', $data);
     }
 
     public function get_list(){
@@ -97,41 +51,22 @@ class Items_Model extends Reim_Model {
     }
 
     public function get_exports($id, $mail){
-        $jwt = $this->session->userdata('jwt');
-        if(!$jwt) return false;
         $data = array(
-            'rid' => $id
-            ,'email' => $mail
+            'rid' => $id,
+            'email' => $mail
         );
-        $url = $this->get_url('exports/' . $id);
-        $buf = $this->do_Post($url, $data, $jwt);
-        log_message("debug", $buf);
-        $obj = json_decode($buf, true);
-        return $obj;
+        return $this->api_post('exports/' . $id, $data);
     }
 
     public function get_suborinate($me = 0, $filter='all'){
-        $jwt = $this->session->userdata('jwt');
-        if(!$jwt) return false;
-        $url = $this->get_url('subordinate_reports/'. $me . "/0/9999999");
-        $url = $url . '?' . http_build_query(['filter' => $filter]);
-        $buf = $this->do_Get($url, $jwt);
-        //log_message("debug", $buf);
-        $obj = json_decode($buf, true);
-        return $obj;
+        $query = ['filter' => $filter];
+        return $this->api_get('subordinate_reports/'. $me . "/0/9999999", null, $query);
     }
 
     public function upload_image($image_path, $type){
-        $jwt = $this->session->userdata('jwt');
-        $file = realpath($image_path);
-        if(!$jwt) return false;
-        $data = array();
         $file = $this->get_curl_upload_field($image_path);
         $data = array('file' => $file, 'type' => $type);
-        $url = $this->get_url('images');
-        $buf = $this->do_Post($url, $data, $jwt, 1);
-        $obj = json_decode($buf, true);
-        return $obj;
+        return $this->api_post('images', $data);
     }
 
     public function create($data)
@@ -160,31 +95,21 @@ class Items_Model extends Reim_Model {
             'customization' => $data['customization']
         );
         array_push($items, $s);
-        if(array_key_exists('rid',$data)){
+        if(array_key_exists('rid', $data)){
             $_data['rid'] = $data['rid'];
         }
         $_data['items'] = json_encode($items);
-        $obj = $this->api_post('item', $_data);
-        return $obj;
+        return $this->api_post('item', $_data);
     }
 
     public function remove($id = 0){
         if($id == 0) return false;
         return $this->api_delete('item/'. $id);
-        return $obj;
     }
 
     public function get_by_id($id = 0){
         if(0 === $id) return array();
-        $jwt = $this->session->userdata('jwt');
-        $url = $this->get_url('item/'. $id);
-        $data = array();
-        $buf = $this->do_Get($url, $jwt);
-        log_message('debug', 'get_item_url:' . $url);
-        log_message('debug', 'get_item_back:' . $buf);
-        $obj = json_decode($buf, true);
-        return $obj;
-
+        return $this->api_get('item/'. $id);
     }
 
     public function update($data){
@@ -214,20 +139,15 @@ class Items_Model extends Reim_Model {
         );
         array_push($items, $s);
         $data = array('items' => json_encode($items));
-        log_message('debug','update_item_data:' . json_encode($data));
+        //log_message('debug','update_item_data:' . json_encode($data));
         $obj = $this->api_put('item', $data);
-        log_message('debug','update_item_back:' . json_encode($obj));
+        //log_message('debug','update_item_back:' . json_encode($obj));
         return $obj;
     }
 
     public function item_flow($iid){
         if(0 === $iid) return array();
-        $jwt = $this->session->userdata('jwt');
-        if(!$jwt) return false;
-        $url = $this->get_url("item_flow/". $iid);
-        $buf = $this->do_Get($url, $jwt);
-        $obj = json_decode($buf, true);
-        return $obj;
+        return $this->api_get("item_flow/". $iid);
     }
 
 }
