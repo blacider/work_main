468fe49033617e1aa76703998527084e3b6b3f9e
George Xie <georgexsh@gmail.com>
2016-05-30 21:06:27 +0800

    use api_* in Template_model

--- application/models/Template_model.php
+++ application/models/Template_model.php
@@ -1,19 +1,9 @@
 <?php
 
 class Template_Model extends Reim_Model{
-    public function __construct() {
-        parent::__construct();
-    }
 
     public function get_template($template_id) {
-
-        $jwt = $this->session->userdata('jwt');
-         
-        $url = $this->get_url('report_template/' . $template_id);
-
-        $buf = $this->do_Get($url, $jwt);
-
-        return json_decode($buf, true);
+        return $this->api_get('report_template/' . $template_id);
     }
 }
 
